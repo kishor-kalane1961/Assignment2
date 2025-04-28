34f369fae41c5cf6f68ca3b0dd3290e82d30e2a0
Kishor Macchindra Kalane <kishorkalane2005@gmail.com>
2025-04-28 21:14:08 +0530

    Update Assign2.py

--- Assign2.py
+++ Assign2.py
@@ -1,3 +1,10 @@
+/*  Implement all the functions of a dictionary (ADT) using hashing 
+and handle collisions using chaining with / without replacement. 
+Data: Set of (key, value) pairs, Keys are mapped to values, Keys 
+must be comparable, Keys must be unique 
+Standard Operations: Insert(key, value), Find(key), Delete(key)*/
+
+
 hashtable = [] 
 size = 0 
 bucket = {} 
